1eafafbe9923be06945b0d5a0688ee685a342f85
Cristian Bastidas <cbastid.dr@gmail.com>
2022-10-17 18:16:13 -0300

    ejercicio video previus

---
+++ script.js
@@ -0,0 +1,25 @@
+
+async function playVideo(videoElem) {
+    try {
+        await videoElem.play();
+    }
+    catch (err) {
+        console.log('error')
+    }
+}
+
+
+function fnOnMouseOver(videoElem) {
+    if (videoElem.paused) {
+        playVideo(videoElem);
+    }
+    else {
+        videoElem.pause();
+    }
+}
+
+function fnOnMouseOut(obj) {
+    let video=document.getElementById("myVideoPlayer");
+    video.pause();
+    video.currentTime=0;
+}
